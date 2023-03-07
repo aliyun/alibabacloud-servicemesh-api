459851a05db9455510499f0d4e094d80151a7f84
露营 <luying.yh@alibaba-inc.com>
2023-03-07 17:54:19 +0800

    merge from istio/api

---
+++ gen.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Copyright Istio Authors
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+set -eu
+
+# Sync API versions
+scripts/sync.sh
+
+# Generate all protos
+buf generate \
+  --path networking \
+  --path security \
+  --path type \
+  --path analysis \
+  --path authentication \
+  --path meta \
+  --path telemetry \
+  --path extensions \
+  --path alibabacloud
+
+# These folders do not have the full plugins used, as they are not full CRDs.
+# We pass them a custom configuration to exclude the non-required files
+buf generate --template buf.gen-noncrd.yaml \
+  --path operator \
+  --path mcp \
+  --path mesh
+
+# These plugins are sent to Envoy, which uses golang/protobuf, so do not use gogo
+buf generate --template buf.gen-golang.yaml \
+  --path envoy
+
+# Generate CRDs and open-api schema
+cue-gen -paths=common-protos -f=./cue.yaml
+cue-gen -paths=common-protos -f=./cue.yaml --crd=true -snake=jwksUri,apiKeys,apiSpecs,includedPaths,jwtHeaders,triggerRules,excludedPaths,mirrorPercent
---
+++ go.mod
@@ -0,0 +1,31 @@
+module istio.io/api
+
+go 1.18
+
+require (
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/genproto v0.0.0-20220628213854-d9e0b6570c03
+	google.golang.org/grpc v1.47.0
+	google.golang.org/protobuf v1.28.0
+	k8s.io/api v0.24.1
+	k8s.io/apimachinery v0.24.1
+)
+
+require (
+	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.60.1 // indirect
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+)
