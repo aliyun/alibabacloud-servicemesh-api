bd2ca5256dbda8031e8526e48a91d6fd766806e9
露营 <luying.yh@alibaba-inc.com>
2024-12-23 19:13:42 +0800

    temp

--- gen-asm-client.sh
+++ gen-asm-client.sh
@@ -1,2 +1,8 @@
-client-gen --clientset-name clientset -h ./hack/boilerplate.go.txt --input-base istio.io/api --input-dirs alibabacloud-servicemesh/v1 --input alibabacloud-servicemesh/v1 --output-base ../alibabacloud-servicemesh-go-client --output-package asm/pkg
+client-gen --clientset-name clientset -h ./hack/boilerplate.go.txt --input-base istio.io/api --input-dirs alibabacloudservicemesh/v1 --input alibabacloudservicemesh/v1 --output-base ../alibabacloud-servicemesh-go-client --output-package asm/pkg
+
+lister-gen -h ./hack/boilerplate.go.txt --input-dirs ./alibabacloudservicemesh/v1 --output-base ../alibabacloud-servicemesh-go-client --output-package asm/pkg/listers
+
+informer-gen --listers-package asm/pkg/listers -h ./hack/boilerplate.go.txt --input-dirs ./alibabacloudservicemesh/v1 --output-base ../alibabacloud-servicemesh-go-client --output-package asm/pkg/informers --versioned-clientset-package  asm/pkg/clientset
+
+find ../alibabacloudservicemesh-go-client/asm/pkg -type f -exec sed -i '' 's#\./alibabacloudservicemesh/#istio.io/api/alibabacloudservicemesh/#g' {} +
 find ../alibabacloud-servicemesh-go-client/asm/pkg -type f -exec sed -i '' 's#asm/pkg/#istio.io/client-go/asm/pkg/#g' {} +
