ca9b35fcd92b00def51c6283a97cbfa4003c0d4f
Kuromesi <blackfacepan@163.com>
2025-02-19 15:20:10 +0800

    update alibabacloud servicemesh apis
    Signed-off-by: Kuromesi <blackfacepan@163.com>

--- alibabacloudservicemesh/v1/asmegresstrafficpolicy_types.go
+++ alibabacloudservicemesh/v1/asmegresstrafficpolicy_types.go
@@ -1,17 +1,3 @@
-// Copyright 2023 Alibaba Cloud Service Mesh
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-
 package v1
 
 import (
@@ -96,12 +82,9 @@ type EgressTrafficPolicyStatus struct {
 //+kubebuilder:printcolumn:name="ByEgressGateway",type=string,JSONPath=`.spec.byEgressGateway.name`
 //+kubebuilder:printcolumn:JSONPath=".status.message",name=status,type=string
 //+kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`
-// +kubebuilder:storageversion
-
-// +genclient
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // ASMEgressTrafficPolicy is the Schema for the ASMEgressTrafficPolicy API
+// +genclient
 type ASMEgressTrafficPolicy struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -112,7 +95,6 @@ type ASMEgressTrafficPolicy struct {
 
 //+kubebuilder:object:root=true
 
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // ASMEgressTrafficPolicyList contains a list of ASMEgressTrafficPolicy
 type ASMEgressTrafficPolicyList struct {
 	metav1.TypeMeta `json:",inline"`
