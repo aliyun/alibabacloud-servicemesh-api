ecb5f98a0fdacc8a9b4fc58afa6b118ec0575c7c
Kuromesi <blackfacepan@163.com>
2025-04-27 15:47:07 +0800

    update asm apis
    Signed-off-by: Kuromesi <blackfacepan@163.com>

--- alibabacloudservicemesh/v1/asmcircuitbreaker_types.go
+++ alibabacloudservicemesh/v1/asmcircuitbreaker_types.go
@@ -1,31 +1,24 @@
-// Copyright 2023 Alibaba Cloud Service Mesh
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
+/*
 
-package v1beta1
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
 
 import (
-	"fmt"
-	"math"
-
-	"github.com/gogo/protobuf/proto"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ = proto.Marshal
-var _ = fmt.Errorf
-var _ = math.Inf
 
 type CircuitBreakerMatch struct {
 	VHost *VirtualHostMatch `protobuf:"bytes,1,opt,name=vhost,proto3" json:"vhost,omitempty"`
@@ -47,11 +40,12 @@ type BreakerConfig struct {
 	//+kubebuilder:validation:Type=string
 	AverageRequestRT *string `json:"average_request_rt,omitempty"`
 	//+kubebuilder:validation:Type=string
-	SlowRequestRT    *string         `json:"slow_request_rt,omitempty"`
-	MaxSlowRequests  *uint32         `json:"max_slow_requests,omitempty"`
-	ErrorPercent     *Percent        `json:"error_percent,omitempty"`
-	MinRequestAmount *uint32         `json:"min_request_amount,omitempty"`
-	CustomResponse   *CustomResponse `json:"custom_response,omitempty"`
+	SlowRequestRT       *string         `json:"slow_request_rt,omitempty"`
+	MaxSlowRequests     *uint32         `json:"max_slow_requests,omitempty"`
+	ErrorPercent        *Percent        `json:"error_percent,omitempty"`
+	GatewayErrorPercent *Percent        `json:"gateway_error_percent,omitempty"`
+	MinRequestAmount    *uint32         `json:"min_request_amount,omitempty"`
+	CustomResponse      *CustomResponse `json:"custom_response,omitempty"`
 	//+kubebuilder:validation:Type=string
 	BreakDuration *string `json:"break_duration,omitempty"`
 	//+kubebuilder:validation:Type=string
@@ -64,10 +58,6 @@ type CircuitBreakerConfig struct {
 	Match *CircuitBreakerMatch `protobuf:"bytes,2,opt,name=match,proto3" json:"match"`
 	//+kubebuilder:validation:Required
 	Breaker *BreakerConfig `protobuf:"bytes,3,opt,name=breaker_config,proto3" json:"breaker_config"`
-
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
 }
 
 // ASMCircuitBreakerSpec defines the desired state of ASMCircuitBreaker
@@ -96,10 +86,8 @@ type ASMCircuitBreakerStatus struct {
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
-// +genclient
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
 // ASMCircuitBreaker is the Schema for the asmcircuitbreaker API
+// +genclient
 type ASMCircuitBreaker struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -109,7 +97,6 @@ type ASMCircuitBreaker struct {
 }
 
 //+kubebuilder:object:root=true
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // ASMCircuitBreakerList contains a list of ASMCircuitBreaker
 type ASMCircuitBreakerList struct {
