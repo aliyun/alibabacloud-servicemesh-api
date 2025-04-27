56b11a4827194a77cc6a92ca777661fd2f9626b5
Kuromesi <blackfacepan@163.com>
2025-04-27 15:47:07 +0800

    update asm apis
    Signed-off-by: Kuromesi <blackfacepan@163.com>

--- alibabacloudservicemesh/v1/asmcompressorgzip_types.go
+++ alibabacloudservicemesh/v1/asmcompressorgzip_types.go
@@ -14,18 +14,7 @@ limitations under the License.
 
 package v1
 
-import (
-	"fmt"
-	"github.com/gogo/protobuf/proto"
-	"math"
-)
-
 // refer to https://github.com/envoyproxy/go-control-plane/blob/84c5a14eb8c8b5b99efeecd3801fea84d628852d/envoy/extensions/filters/http/compressor/v3/compressor.pb.go
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ = proto.Marshal
-var _ = fmt.Errorf
-var _ = math.Inf
 
 type GzipCompressor struct {
 	// Value from 1 to 9 that controls the amount of internal memory used by zlib. Higher values
--- scripts/gen-asm-clients.sh
+++ scripts/gen-asm-clients.sh
@@ -5,11 +5,15 @@ set -e
 
 OUTPUT_BASE="${OUTPUT_BASE:-generated-clients}"
 
-bin/client-gen --clientset-name clientset -h ./hack/boilerplate.go.txt --input-base istio.io/api --input-dirs alibabacloudservicemesh/v1 --input alibabacloudservicemesh/v1 --output-base $OUTPUT_BASE --output-package asm/pkg
+INPUT="alibabacloudservicemesh/v1,alibabacloudservicemesh/v1beta1"
 
-bin/lister-gen -h ./hack/boilerplate.go.txt --input-dirs ./alibabacloudservicemesh/v1 --output-base $OUTPUT_BASE --output-package asm/pkg/listers
+INPUTS="./alibabacloudservicemesh/v1,./alibabacloudservicemesh/v1beta1"
 
-bin/informer-gen --listers-package asm/pkg/listers -h ./hack/boilerplate.go.txt --input-dirs ./alibabacloudservicemesh/v1 --output-base $OUTPUT_BASE --output-package asm/pkg/informers --versioned-clientset-package  asm/pkg/clientset
+bin/client-gen --clientset-name versioned -h ./hack/boilerplate.go.txt --input-base "istio.io/api" --input-dirs $INPUTS --input $INPUT --output-base $OUTPUT_BASE --output-package asm/pkg/clientset
+
+bin/lister-gen -h ./hack/boilerplate.go.txt --input-dirs $INPUTS --output-base $OUTPUT_BASE --output-package asm/pkg/listers
+
+bin/informer-gen --listers-package asm/pkg/listers -h ./hack/boilerplate.go.txt --input-dirs $INPUTS --output-base $OUTPUT_BASE --output-package asm/pkg/informers --versioned-clientset-package asm/pkg/clientset/versioned
 
 find $OUTPUT_BASE/asm/pkg -type f -exec sed -i 's#\./alibabacloudservicemesh/#istio.io/api/alibabacloudservicemesh/#g' {} +
 find $OUTPUT_BASE/asm/pkg -type f -exec sed -i 's#asm/pkg/#istio.io/client-go/asm/pkg/#g' {} +
