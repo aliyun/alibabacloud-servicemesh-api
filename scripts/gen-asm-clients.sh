6283cb09619af427bb2e43fc0bd622ce9e46e4ec
Kuromesi <blackfacepan@163.com>
2025-06-26 14:32:32 +0800

    fix ampere client gen issues
    Signed-off-by: Kuromesi <blackfacepan@163.com>

--- scripts/gen-asm-clients.sh
+++ scripts/gen-asm-clients.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 set -x
-set -e
+# set -e
 
 OUTPUT_BASE="${OUTPUT_BASE:-generated-clients}"
 
-INPUT="alibabacloudservicemesh/v1,alibabacloudservicemesh/v1beta1"
+INPUT="alibabacloudservicemesh/v1,alibabacloudservicemesh/v1beta1,ampere/v1"
 
-INPUTS="./alibabacloudservicemesh/v1,./alibabacloudservicemesh/v1beta1"
+INPUTS="./alibabacloudservicemesh/v1,./alibabacloudservicemesh/v1beta1,./ampere/v1"
 
-bin/client-gen --clientset-name versioned -h ./hack/boilerplate.go.txt --input-base "istio.io/api" --input-dirs $INPUTS --input $INPUT --output-base $OUTPUT_BASE --output-package asm/pkg/clientset
+bin/client-gen --clientset-name versioned \
+    --go-header-file ./hack/boilerplate.go.txt \
+    --output-base $OUTPUT_BASE  \
+    --input-base "istio.io/api" \
+    --input $INPUT \
+    --output-package asm/pkg/clientset \
 
-bin/lister-gen -h ./hack/boilerplate.go.txt --input-dirs $INPUTS --output-base $OUTPUT_BASE --output-package asm/pkg/listers
+bin/lister-gen --go-header-file ./hack/boilerplate.go.txt \
+    --input-dirs $INPUTS \
+    --output-base $OUTPUT_BASE \
+    --output-package asm/pkg/listers
 
-bin/informer-gen --listers-package asm/pkg/listers -h ./hack/boilerplate.go.txt --input-dirs $INPUTS --output-base $OUTPUT_BASE --output-package asm/pkg/informers --versioned-clientset-package asm/pkg/clientset/versioned
+bin/informer-gen --listers-package asm/pkg/listers \
+    --go-header-file ./hack/boilerplate.go.txt \
+    --input-dirs $INPUTS \
+    --output-base $OUTPUT_BASE \
+    --output-package asm/pkg/informers \
+    --versioned-clientset-package \asm/pkg/clientset/versioned || true
 
 find $OUTPUT_BASE/asm/pkg -type f -exec sed -i 's#\./alibabacloudservicemesh/#istio.io/api/alibabacloudservicemesh/#g' {} +
+find $OUTPUT_BASE/asm/pkg -type f -exec sed -i 's#\./ampere/#istio.io/api/ampere/#g' {} +
 find $OUTPUT_BASE/asm/pkg -type f -exec sed -i 's#asm/pkg/#istio.io/client-go/asm/pkg/#g' {} +
 
 set +e
